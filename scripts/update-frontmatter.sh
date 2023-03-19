bb3c8f112b0398cc12f8b96aa1a89529af22844e
Byron <byronogis@outlook.com>
2023-03-19 14:41:34 +0800

    chore: update  auto update time script

--- scripts/update-frontmatter.sh
+++ scripts/update-frontmatter.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-# Get the current date in ISO 8601 format
 DATE=$(date +"%Y-%m-%d %H:%M")
 
-# Get the names of the Markdown files being committed
+# 获取待提交的 Markdown 文件名
 FILES=$(git diff --cached --name-only --diff-filter=ACM | grep '\.md$')
 
-# Loop through each Markdown file and update its Front Matter
+# 遍历每个待提交的 Markdown 文件并更新其 Front Matter
 for FILE in $FILES; do
-  # Check if the file contains Front Matter
+  # 判断文件是否包含 Front Matter
   if grep -q '^---' "$FILE"; then
-    # Update the "update" field in the Front Matter if it exists
-    if grep -q 'update:' "$FILE"; then
-      sed -i "s/\(update:\s*\).*/\1$DATE/" "$FILE"
+    # 使用 awk 在第二个 '---' 行之前的内容，匹配 'update' 字段
+    FRONT_MATTER=$(awk '/^---/{n++;next} n==2{exit} {print}' "$FILE")
+    if echo "$FRONT_MATTER" | grep -q 'update:'; then
+      # 更新 'update' 字段的时间戳
+      sed -i "0,/update:/s/\(update:\s*\).*/\1$DATE/" "$FILE"
     else
-      # Insert a new "update" field in the Front Matter
-      sed -i "s/\(---\)/\1\nupdate: $DATE/" "$FILE"
+      # 如果 Front Matter 中不存在 'update' 字段，则在第一个 '---' 行之后添加
+      sed -i "0,/^---/{/^---/a update: $DATE
+      :b;n;bb}" "$FILE"
     fi
   else
-    # Insert a new Front Matter with the "update" field
+    # 如果文件没有 Front Matter，则在开头添加 Front Matter 和 'update' 字段
     sed -i "1i---\nupdate: $DATE\n---\n" "$FILE"
   fi
 done
