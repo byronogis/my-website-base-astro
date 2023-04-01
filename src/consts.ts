559b21f1509ad3351b5ab1c95235e2ed2f76b896
Byron <byronogis@outlook.com>
2023-04-01 17:11:58 +0800

    feat: responsive header

--- src/consts.ts
+++ src/consts.ts
@@ -4,13 +4,13 @@
 export const SITE_TITLE = '天有多云转晴'
 export const SITE_DESCRIPTION = 'Welcome to my website!'
 export const SITE_NAVIGATION = [
-  { value: 'Home', href: '/' },
-  { value: 'Essay', href: '/essay' },
-  { value: 'Tags', href: '/tags' },
+  { value: 'Home', href: '/', pin: false },
+  { value: 'Essay', href: '/essay', pin: true },
+  { value: 'Tags', href: '/tags', pin: true },
   // { value: 'Project', href: '/project' },
-  { value: 'Icons', href: '/icons' },
-  { value: 'About', href: '/about' },
-  { value: 'Github', href: 'https://github.com/liamrad', external: true },
+  { value: 'Icons', href: '/icons', pin: false },
+  { value: 'About', href: '/about', pin: false },
+  { value: 'Github', href: 'https://github.com/liamrad', external: true, pin: false },
 ]
 export const SITE_POST_TAGS: [string, ...string[]] = [
   'no tags',
