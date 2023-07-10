6abb9f6bc145e27498cb6a03833f854e320a539b
Byron <byronogis@outlook.com>
2023-07-10 15:46:13 +0800

    chore: config manual page

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,37 +1,48 @@
 import { defineCollection, z } from 'astro:content'
 // import { SITE_POST_TAGS } from '~/consts'
 
-const essay = defineCollection({
+const basicSchemaObject = {
+  title: z.string(),
+  description: z
+    .string()
+    .nullable()
+    .transform(val => val ?? 'no description'),
+  tags: z
+    // .enum(SITE_POST_TAGS)
+    .string()
+    .array()
+    .optional(),
+  // Transform string to Date object
+  date: z
+    .string()
+    .or(z.date())
+    .transform(val => new Date(val)),
+  update: z
+    .string()
+    .or(z.date())
+    .optional()
+    .transform(val => val ? new Date(val) : undefined),
+  //
+  heroImage: z.string().optional(),
+  head: z
+    .tuple([z.string(), z.record(z.string(), z.string())])
+    .array()
+    .optional(),
+}
+
+const essayCollection = defineCollection({
   schema: z.object({
-    title: z.string(),
-    description: z
-      .string()
-      .nullable()
-      .transform(val => val ?? 'no description'),
-    tags: z
-      // .enum(SITE_POST_TAGS)
-      .string()
-      .array()
-      .optional(),
-    // Transform string to Date object
-    date: z
-      .string()
-      .or(z.date())
-      .transform(val => new Date(val)),
-    update: z
-      .string()
-      .or(z.date())
-      .optional()
-      .transform(val => val ? new Date(val) : undefined),
-    //
-    heroImage: z.string().optional(),
-    head: z
-      .tuple([z.string(), z.record(z.string(), z.string())])
-      .array()
-      .optional(),
+    ...basicSchemaObject,
+  }),
+})
+
+const manualCollection = defineCollection({
+  schema: z.object({
+    ...basicSchemaObject,
   }),
 })
 
 export const collections = {
-  essay,
+  essay: essayCollection,
+  manual: manualCollection,
 }
---
+++ src/content/manual/example.md
@@ -0,0 +1,19 @@
+---
+update: 2023-07-10 15:46
+title: example
+description: example
+date: 2023-03-26 22:00
+tags: [astro]
+head:
+  - - meta
+    - name: keywords
+      content: astro
+---
+
+## 目录
+
+## 二级标题
+
+### 三级标题
+
+## 二级标题
